1990bb8d6c8b882d3fd6277b35df1ae4e14ab335
FgSurewin <mrliu3244794@gmail.com>
2020-09-24 11:53:59 -0400

    configuration setup

--- react__typescript_demo/src/App.tsx
+++ react__typescript_demo/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Button } from "antd";
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>Welcome to React in TypeScript</h1>
+			<Button type='primary'>ADD</Button>
 		</div>
 	);
 }
